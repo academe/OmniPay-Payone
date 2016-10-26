4f2f9a6ef255d2fb57be6fcc9e68fbb976044007
artemskiy <garcia82@yandex.ru>
2016-10-26 18:06:08 +0300

    fix for array encoding

--- src/Message/ShopTransactionStatusServerRequest.php
+++ src/Message/ShopTransactionStatusServerRequest.php
@@ -66,7 +66,11 @@ class ShopTransactionStatusServerRequest extends OmnipayAbstractRequest implemen
 
             array_walk($data, function (&$item) {
                 if (!empty($item)) {
-                    $item = utf8_encode($item);
+                    if (is_array($item)) {
+                        $item = json_decode(utf8_encode(json_encode($item)), true);
+                    } else {
+                        $item = utf8_encode($item);
+                    }
                 }
             });
         }

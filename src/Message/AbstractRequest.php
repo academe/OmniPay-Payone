763182e7ca900bf603e249d90e1d4b474cab29f5
Jason Judge <jason.judge@consil.co.uk>
2017-06-13 12:26:00 +0100

    Issue #27 I take issue with this CS rule, but let's just get it throug Travis.

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -725,11 +725,11 @@ abstract class AbstractRequest extends OmnipayAbstractRequest
                 $card->addSupportedBrand(
                     'discover',
                     '^6('
-                        . '?:011\d{12}'
-                        . '|5\d{14}'
-                        . '|4[4-9]\d{13}'
-                        . '|22(?:1(?:2[6-9]|[3-9]\d)|[2-8]\d{2}|9(?:[01]\d|2[0-5]))\d{10}'
-                     . ')$'
+                    . '?:011\d{12}'
+                    . '|5\d{14}'
+                    . '|4[4-9]\d{13}'
+                    . '|22(?:1(?:2[6-9]|[3-9]\d)|[2-8]\d{2}|9(?:[01]\d|2[0-5]))\d{10}'
+                    . ')$'
                 );
 
                 // No regex found for Carte Bleue cards.
